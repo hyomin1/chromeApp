2c28d8b42353e99e66355be32896b83d79aa07c2
hyomin <leehm123456@gmail.com>
2023-01-28 19:18:23 +0900

    js complete

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,25 @@
+const API_KEY = "bd51d3ba07674224c94a1dc9cdfe9c1d";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
-    console.log(lat,long);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data=> {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }); //js가 url요청 ,promise
 }
 
 function onGeoError() {
     alert("Can't find you. No weather for you");
 }
 
-function a() {
-    
-}
 
+function a() {
+    console.log("a");
+    console.log("b");
+}
 navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError); 
